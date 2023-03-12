4facc7d855df86075902c8a821c8fb2d2c529669
Jeffery Hutchinson <jefferyshutchinson@gmail.com>
2023-03-12 10:57:06 -0400

    Amazon uses a ton of employee tracking!

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = Object.assign({}, employee);
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  const employee = {
+    name: "Sam",
+    streetAddress: "123 Main St."
+  };
+  
+  const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "email", "sam@company.com");
+  console.log(updatedEmployee); // { name: "Sam", streetAddress: "123 Main St.", email: "sam@company.com" }
+  
+  destructivelyUpdateEmployeeWithKeyAndValue(employee, "email", "sam@company.com");
+  console.log(employee); // { name: "Sam", streetAddress: "123 Main St.", email: "sam@company.com" }
+  
+  const newEmployee = deleteFromEmployeeByKey(employee, "streetAddress");
+  console.log(newEmployee); // { name: "Sam", email: "sam@company.com" }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
